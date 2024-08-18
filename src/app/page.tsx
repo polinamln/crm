0801ce69d28480e9dc2b52d017fe4f75050a2cb3
Polina Arkhypenko <137650718+polinamln@users.noreply.github.com>
2024-08-18 22:11:28 +0300

    module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import ActiveLabel from './components/active-label';
+import NotActiveLabel from './components/not-active-label';
 
 export default function Home() {
   return (
-    <main >
-     <h1>Home</h1>
+    <main>
+      <h1>Home page</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
   );
 }
